7f366d4bd1680c91f7895f204b54656e27fa8ced
vladro01 <vladro014@gmail.com>
2025-11-13 23:19:41 +0000

    Berties Lab work

--- routes/books.js
+++ routes/books.js
@@ -10,6 +10,17 @@ router.get('/search-result', function (req, res, next) {
     //searching in the database
     res.send("You searched for: " + req.query.keyword)
 });
+router.get('/list', function(req, res, next) {
+    let sqlquery = "SELECT * FROM books"; // query database to get all the books
+    // execute sql query
+    db.query(sqlquery, (err, result) => {
+        if (err) {
+            next(err)
+        }
+        res.send(result)
+     });
+});
+
 
 // Export the router object so index.js can access it
 module.exports = router
