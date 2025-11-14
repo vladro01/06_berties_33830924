85fc082fe14d863a1c6fa4ace151612a4c5c859a
vladro01 <vladro014@gmail.com>
2025-11-14 00:10:20 +0000

    Some new updates

--- routes/books.js
+++ routes/books.js
@@ -7,8 +7,16 @@ router.get('/search',function(req, res, next){
 });
 
 router.get('/search-result', function (req, res, next) {
-    //searching in the database
-    res.send("You searched for: " + req.query.keyword)
+    let keyword = req.query.keyword;
+    let sqlquery = "SELECT * FROM books WHERE name LIKE ?";
+    let params = ['%' + keyword + '%'];
+
+    db.query(sqlquery, params, (err, result) => {
+        if (err) {
+            return next(err);
+        }
+        res.render('list.ejs', { availableBooks: result });
+    });
 });
 router.get('/list', function(req, res, next) {
     let sqlquery = "SELECT * FROM books"; // query database to get all the books
@@ -17,8 +25,37 @@ router.get('/list', function(req, res, next) {
         if (err) {
             next(err)
         }
-        res.send(result)
+        res.render("list.ejs", { availableBooks: result });
      });
+});
+// Show the add book form
+router.get('/addbook', function (req, res, next) {
+    res.render('addbook.ejs');
+});
+
+// Handle form submit and insert into DB
+router.post('/bookadded', function (req, res, next) {
+    let sqlquery = "INSERT INTO books (name, price) VALUES (?, ?)";
+    let newrecord = [req.body.name, req.body.price];
+
+    db.query(sqlquery, newrecord, (err, result) => {
+        if (err) {
+            return next(err);
+        }
+        res.send('This book is added to database, name: ' +
+            req.body.name + ' price ' + req.body.price);
+    });
+});
+//Extensions
+router.get('/bargainbooks', function (req, res, next) {
+    let sqlquery = "SELECT * FROM books WHERE price < 20";
+
+    db.query(sqlquery, (err, result) => {
+        if (err) {
+            return next(err);
+        }
+        res.render('list.ejs', { availableBooks: result });
+    });
 });
 
 
